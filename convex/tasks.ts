6a15d6f4ebee2030752335c9659c8d2ac8563f95
Thabiso <tmosenohi99@gmail.com>
2024-11-01 12:39:41 +0200

    Added 'add task functinality'

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,8 +1,18 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("tasks").collect();
   },
+});
+
+
+
+export const add = mutation({
+  args: { text: v.string() },
+  handler: async ({ db }, { text }) => {
+    await db.insert("tasks", { text });
+  }
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import { useState } from 'react'
-import { useQuery } from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "../convex/_generated/api";
 
 import './App.css'
 
 function App() {
+  const [text, setText] = useState("");
   const tasks = useQuery(api.tasks.get);
+  const addTask = useMutation(api.tasks.add);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (text) {
+      await addTask({ text });
+      setText(""); 
+    }
+  };
   return (
     <>
+    <div>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={text} 
+            onChange={(e) => setText(e.target.value)} 
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
       <div>
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}      </div>
-      
+        {tasks?.map(({ _id, text }) => (
+          <div key={_id}>{text}</div>
+        ))}
+      </div>
     </>
   )
+
 }
 
 export default App
